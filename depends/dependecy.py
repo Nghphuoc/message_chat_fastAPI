c16b3569506881669078219ca8d92218867a292c
data Test <phuocvk9000@gmail.com>
2025-06-12 08:54:40 +0700

    update: role dependecy

--- depends/dependecy.py
+++ depends/dependecy.py
@@ -2,7 +2,9 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from dbconfig.config import get_db
+from repository.RoleRepository import RoleRepository
 from repository.UserRepository import UserRepository
+from service.RoleService import RoleService
 from service.UserService import UserService
 
 
@@ -10,3 +12,8 @@ from service.UserService import UserService
 def user_service(db: Session = Depends(get_db)):
     repo = UserRepository(db)
     return UserService(repo)
+
+# role service
+def role_service(db: Session = Depends(get_db)):
+    repo = RoleRepository(db)
+    return RoleService(repo)
