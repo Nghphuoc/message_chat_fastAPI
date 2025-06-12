bb11095b5a3432df339a6c7df539ca2af5a31e5b
data Test <phuocvk9000@gmail.com>
2025-06-12 08:56:12 +0700

    modify: column role_name

--- model/Role.py
+++ model/Role.py
@@ -14,6 +14,6 @@ class Role(Base):
     __tablename__ = 'TB_ROLES'
 
     role_id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()), nullable=False)
-    role = Column(SqlEnum(RoleType), default = RoleType.MODERATOR, nullable=False)
+    role_name = Column(SqlEnum(RoleType), default = RoleType.MODERATOR, nullable=False)
 
     user = relationship("Users", back_populates="role")
