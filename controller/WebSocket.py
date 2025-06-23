52127e6dd8d2e79df37ff530135539d2d4695ee2
PhuocHN <huuphuocit04@gmail.com>
2025-06-23 13:33:29 +0700

    modify: WebSocket.py new feature set status login

--- controller/WebSocket.py
+++ controller/WebSocket.py
@@ -3,13 +3,13 @@ import json
 from cachetools import TTLCache
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends, HTTPException
 import asyncio
-from model.schema import MessageRequest, UserResponse
+from model.schema import MessageRequest, UserResponse, UserInRoomResponse
 from redis.redis_manager import RedisPubSub
-from depends.dependecy import user_status_service, message_service, user_service
-from service import StatusService, MessageService
+from depends.dependecy import user_status_service, message_service, user_service, user_room_service
+from service import StatusService, MessageService, UserRoomService
 import logging
 
-
+from service.MessageService import to_vietnam_time
 from service.UserService import UserService
 
 logger = logging.getLogger(__name__)
@@ -92,7 +92,7 @@ async def websocket_endpoint(websocket: WebSocket, room_id: str, user_id: str,
             service.update_status(user_id=user_id, is_online=False)
             connected_clients[room_id].discard(websocket)
             if not connected_clients[room_id]:
-                service.update_status(user_id, False)  # 👉 OFFLINE
+                service.update_status(user_id=user_id, is_online=False)
                 # Nếu phòng không còn ai, hủy subscribe hoặc xóa key
                 connected_clients.pop(room_id, None)
 
@@ -123,3 +123,55 @@ async def websocket_endpoint(websocket: WebSocket, room_id: str, user_id: str,
                 connected_clients.pop(room_id, None)
 
     await asyncio.gather(receive_ws(), send_ws())
+
+
+# # call ws get room and list chat of user
+# @ws_router.websocket("/status/{user_id}")
+# async def get_all_room_of_user(user_id: str, websocket: WebSocket,
+#                                service_user_room: UserRoomService = Depends(user_room_service),
+#                                user_service: UserService = Depends(user_service),
+#                                user_status: StatusService = Depends(user_status_service)):
+#
+#     await websocket.accept()
+#     user_status.update_status(user_id=user_id, is_online=True)
+#     print(f"[ONLINE] User {user_id} is now online.")
+#
+#     try:
+#         while True:
+#             room_ids = service_user_room.get_all_list_room_for_user(user_id)
+#             result = []
+#              # step 1: get room of user
+#             for room_tuple in room_ids:
+#                 room_id = room_tuple[0]
+#                 # step 1.1: call service ( include user_id and friend_id much filed )
+#                 user_rooms = service_user_room.get_user_id_by_room_id(room_id)
+#                 # step 1.2: get user_id
+#                 for user_room in user_rooms:
+#                     if str(user_room.user_id) != str(user_id):
+#                         user_info = user_service.get_user_by_id(user_room.user_id)
+#                         if not user_info:
+#                             continue
+#
+#                         status_user = user_status.get_status_by_user(user_info.user_id)
+#                         display_name = user_info.display_name or user_info.username
+#                         # step 1.3: add list to return
+#                         result.append(UserInRoomResponse(
+#                             img_url=user_info.img_url,
+#                             username=display_name,
+#                             room_id=user_room.room_id,
+#                             status=status_user.is_online if status_user else False,
+#                             last_seen=to_vietnam_time(status_user.last_seen) if status_user else None,
+#                         ))
+#
+#             # Gửi danh sách phòng cho client mỗi 10 giây
+#             await websocket.send_json([r.dict() for r in result])
+#             await asyncio.sleep(10)
+#
+#     except WebSocketDisconnect:
+#         print(f"[DISCONNECT] User {user_id} disconnected.")
+#         user_status.update_status(user_id=user_id, is_online=False)
+#
+#     except Exception as e:
+#         print(f"[ERROR] WebSocket error for {user_id}: {e}")
+#         user_status.update_status(user_id=user_id, is_online=False)
+#         await websocket.close()
