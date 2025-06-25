e054d5568699fd21cfdc5ea4e7c7f6f684532b37
PhuocHN <huuphuocit04@gmail.com>
2025-06-25 11:38:32 +0700

    add: constraint message and user

--- model/Reaction.py
+++ model/Reaction.py
@@ -1,6 +1,6 @@
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, ForeignKey, DateTime, String
+from sqlalchemy import Column, ForeignKey, DateTime, String, UniqueConstraint
 from sqlalchemy.orm import relationship
 from dbconfig.config import Base
 
@@ -12,6 +12,11 @@ class Reaction(Base):
     message_id = Column(String(36), ForeignKey('TB_MESSAGES.message_id'), nullable=False)
     emoji = Column(String(10), nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    # add UNIQUE constraint ở đây
+    __table_args__ = (
+        UniqueConstraint('user_id', 'message_id', name='uniq_user_message'),
+    )
 
     user = relationship("Users", back_populates="reactions")
     message = relationship("Message", back_populates="reactions")
