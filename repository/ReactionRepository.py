2871b5599f3f864b7cbbbb57341674bbd335075e
PhuocHN <huuphuocit04@gmail.com>
2025-06-26 13:50:04 +0700

    fix: logic database function remove and get reaction by id

--- repository/ReactionRepository.py
+++ repository/ReactionRepository.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from model import Reaction
@@ -21,10 +22,11 @@ class ReactionRepository:
 
     def remove_reaction(self, reaction_id: str):
         try:
-            self.db.query(Reaction).filter(Reaction.id == reaction_id).delete()
+            reaction = self.db.query(Reaction).filter(Reaction.reaction_id == reaction_id).first()
+            if not reaction:
+                raise HTTPException(status_code=404, detail="Reaction not found")
+            self.db.delete(reaction)
             self.db.commit()
-            self.db.refresh(Reaction)
-            return True
         except Exception as e:
             raise Exception("ERROR REMOVE REACTION AT ReactionRepository: ", e)
 
@@ -33,19 +35,9 @@ class ReactionRepository:
     def get_reaction_by_message_id(self, message_id: str):
         try:
             results = self.db.query(Reaction).filter(Reaction.message_id == message_id).all()
-
-            if not results:
-                # Không có reaction nào
-                return None
-            elif len(results) == 1:
-                # Có đúng 1 reaction
-                return results[0]
-            else:
-                # Có nhiều reaction
-                return results
-
+            return results  # luôn trả list, dù rỗng hay không
         except Exception as e:
-            raise Exception("ERROR GET REACTION AT ReactionRepository: ", e)
+            raise Exception("ERROR GET REACTION AT ReactionRepository: " + str(e))
 
 
     def get_reaction_by_message_id_and_user_id(self, message_id: str, user_id: str):
