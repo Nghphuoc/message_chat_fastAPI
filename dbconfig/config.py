443556151b4f9993e24082a7c13fc99c8c983117
PhuocHN <huuphuocit04@gmail.com>
2025-07-25 08:54:18 +0700

    fix: hidden source name db connect

--- dbconfig/config.py
+++ dbconfig/config.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
-
 load_dotenv(dotenv_path=os.getenv("ENV_FILE", ".env.prod"))
 
 DB_HOST = os.getenv("DB_HOST")
@@ -10,8 +9,8 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"  # Có thể đổi sang PostgreSQL, MySQL...
+NAME_DB= os.getenv("NAME_DB")
+DATABASE_URL = f"{NAME_DB}{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"  # Có thể đổi sang PostgreSQL, MySQL...
 
 # DATABASE_URL = "mysql+pymysql://root:QKddluOjJFunHBUFoAoSZSIJJCoBVtsv@nozomi.proxy.rlwy.net:30138/railway"
 
