d44bfd8aa2c944235d2c9600493cd2c03f3981f5
PhuocHN <huuphuocit04@gmail.com>
2025-07-30 15:41:13 +0700

    update: check create role

--- dbconfig/config.py
+++ dbconfig/config.py
@@ -23,7 +23,7 @@ engine = create_engine(
     pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine,checkfirst=True)
 
 Base = declarative_base()
 
